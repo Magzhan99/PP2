4bb0f5bebcc69c1cb821bb87de71465e4008719b
Magzhan99 <kadylbekov.m99@gmail.com>
2018-01-14 21:06:54 +0600

    maga maga

---
+++ Week1/prime_numbers/prime_numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            args = n.Split(' ');
+            foreach (string t in args)
+            {
+                int a = int.Parse(t);
+                int ok = 1;
+                for (int j = 2; j <= Math.Sqrt(a); j++)
+                {
+                    if(a % j == 0)
+                    {
+                        ok = 0;
+                    }
+                }
+                if(ok == 1)
+                {
+                    Console.WriteLine(a);
+                    ok = 1;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
