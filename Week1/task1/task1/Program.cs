2800623e73837a99aa784cfddf2dcd4f65f8fc14
Magzhan99 <kadylbekov.m99@gmail.com>
2018-01-14 17:40:34 +0600

    maga maga

---
+++ Week1/task1/task1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+
+    class Student
+    {
+        public string fname;
+        public string lname;
+        public int age;
+        public float GPA;
+
+        public Student()
+        {
+            fname = "magzhan";
+            lname = "kadylbekov";
+            age = 18;
+            GPA = 3; 
+        }
+
+        public override string ToString()
+        {
+            return fname + " " + lname + " " + age + " " + GPA;
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.GPA = 4;
+
+
+            Student s2 = new Student();
+            Console.WriteLine(s);      // magzhan kadylbekov 18 4 
+            Console.WriteLine(s2);     // magzhan kadylbekov 18 3
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
