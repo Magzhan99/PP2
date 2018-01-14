355c0a4fd4d35382ed838492b532169df55c687a
Magzhan99 <kadylbekov.m99@gmail.com>
2018-01-14 14:00:23 +0600

    maga maga

---
+++ Week1/ex/ex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Week1/ex2/ex2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 1; counter <= 16; counter++)
+            {
+                int reminder3 = counter % 3;
+                int reminder5 = counter % 5;
+
+                /*if (reminder3 == 0 && reminder5 == 0)
+                {
+                    System.Console.WriteLine("FizzBuzz");
+                }
+                else */if (reminder3 == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                }
+                if (reminder5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    System.Console.WriteLine(counter);
+                }
+            }
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
