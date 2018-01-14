d251abad23bfaf4518a12862149838df62bc8673
Magzhan99 <kadylbekov.m99@gmail.com>
2018-01-14 21:50:22 +0600

    uaha uaha

---
+++ Week1/circle/circle/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circle
+{
+    class Circle
+    {
+        public int radius;
+        public double area;
+        public int diametr;
+        public double length;
+
+
+        public Circle()
+        {
+            radius = 5;
+        }
+        public Circle(int R)
+        {
+            radius = R;
+        }
+        public void findArea()
+        {
+            area = 3.14 * radius * radius;
+        }
+        public void findLength()
+        {
+            length = 2 * 3.14 * radius;
+        }
+        public void findDiametr()
+        {
+            diametr = 2 * radius;
+        }
+        public override string ToString()
+        {
+            return radius + "\n" + area + "\n" + length + "\n" + diametr;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle s = new Circle(int.Parse(Console.ReadLine()));
+            s.findArea();
+            s.findLength();
+            s.findDiametr();
+
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+
+        }
+    }
+}
